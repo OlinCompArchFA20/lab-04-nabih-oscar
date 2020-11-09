bd98069ceac1f3271bce5b0d4fb390d677befbf8
Oscar De La Garza <odelagarza@olin.edu>
2020-11-09 13:48:59 -0500

    ALU producing UNOPTFLAT ERROR, code updated

--- verilog/alu.v
+++ verilog/alu.v
@@ -1,4 +1,5 @@
 `include "lib/opcodes.v"
+`include "alu_slice.v"
 `timescale 1ns / 1ps
 
 module ALU
@@ -18,23 +19,23 @@ module ALU
 
   always @* begin
     case(alu_op)
-      `ADD:  begin carry[0] = 0; end
+      `F_ADD:  begin carry[0] = 0; end
       `ADDI:  begin carry[0] = 0; end
       `ADDIU:  begin carry[0] = 0; end
-      `ADDU:  begin carry[0] = 0; end
-      `AND:  begin carry[0] = 0; end
+      `F_ADDU:  begin carry[0] = 0; end
+      `F_AND:  begin carry[0] = 0; end
       `ANDI:  begin carry[0] = 0; end
-      `NOR:  begin carry[0] = 0; end
-      `OR:  begin carry[0] = 0; end
+      `F_NOR:  begin carry[0] = 0; end
+      `F_OR:  begin carry[0] = 0; end
       `ORI:  begin carry[0] = 0; end
-      `SLT:  begin carry[0] = 1; end
+      `F_SLT:  begin carry[0] = 1; end
       `SLTI:  begin carry[0] = 1; end
       `SLTIU:  begin carry[0] = 1; end
-      `SLTU:  begin carry[0] = 1; end
-      `SLL:  begin carry[0] = 0; end
-      `SRL:  begin carry[0] = 0; end
-      `SUB:  begin carry[0] = 1; end
-      `SUBU:  begin carry[0] = 1; end
+      `F_SLTU:  begin carry[0] = 1; end
+      `F_SLL:  begin carry[0] = 0; end
+      `F_SRL:  begin carry[0] = 0; end
+      `F_SUB:  begin carry[0] = 1; end
+      `F_SUBU:  begin carry[0] = 1; end
       default : /*Default catch*/;
     endcase
   end
@@ -45,31 +46,31 @@ module ALU
     end
   endgenerate
 
-  assign cout = carry[W];
+  assign cout = carry[`W_CPU];
   //Detects overflow
-  xor #DLY OVERFLOW_G1(ovfl, carry[W], carry[W-1]);
-  xor #DLY OVERFLOW_G2(neg, ovfl, result[W-1]);
+  xor #DLY OVERFLOW_G1(ovfl, carry[`W_CPU], carry[`W_CPU-1]);
+  xor #DLY OVERFLOW_G2(neg, ovfl, result[`W_CPU-1]);
 
   //Assigns overflow for cases
   always @* begin
     case (alu_op)
-      `ADD:  begin overflow = neg; end
+      `F_ADD:  begin overflow = neg; end
       `ADDI:  begin overflow = neg; end
-      `ADDIU:  begin overflow = carry[W]; end
-      `ADDU:  begin overflow = carry[W]; end
-      `AND:  begin overflow = 1'b0; end
+      `ADDIU:  begin overflow = carry[`W_CPU]; end
+      `F_ADDU:  begin overflow = carry[`W_CPU]; end
+      `F_AND:  begin overflow = 1'b0; end
       `ANDI:  begin overflow = 1'b0; end
-      `NOR:  begin overflow = 1'b0; end
-      `OR:  begin overflow = 1'b0; end
+      `F_NOR:  begin overflow = 1'b0; end
+      `F_OR:  begin overflow = 1'b0; end
       `ORI:  begin overflow = 1'b0; end
-      `SLT:  begin overflow = 1'b0; end
+      `F_SLT:  begin overflow = 1'b0; end
       `SLTI:  begin overflow = 1'b0; end
       `SLTIU:  begin overflow = 1'b0; end
-      `SLTU:  begin overflow = 1'b0; end
-      `SLL:  begin overflow = 1'b0; end
-      `SRL:  begin overflow = 1'b0; end
-      `SUB:  begin overflow = neg; end
-      `SUBU:  begin overflow = carry[W]; end
+      `F_SLTU:  begin overflow = 1'b0; end
+      `F_SLL:  begin overflow = 1'b0; end
+      `F_SRL:  begin overflow = 1'b0; end
+      `F_SUB:  begin overflow = neg; end
+      `F_SUBU:  begin overflow = carry[`W_CPU]; end
       default: /* default catch */;
     endcase
   end
@@ -77,7 +78,7 @@ module ALU
   integer j;
   //SLT CHECK
   always @* begin
-    if (alu_op == `SLT) begin
+    if (alu_op == `F_SLT) begin
       if (A == B) begin
         isZero = 0;
       end
@@ -85,7 +86,7 @@ module ALU
         isZero = neg;
       end
     end
-    else if(alu_op == `SLTU) begin
+    else if(alu_op == `F_SLTU) begin
       if (A == B) begin
         isZero = 0;
       end
@@ -93,23 +94,40 @@ module ALU
         isZero = neg;
       end
     end
-    else if (alu_op == `SLL) begin
+    else if (alu_op == `SLTI) begin
+      if (A == B) begin
+        isZero = 0;
+      end
+      else begin
+        isZero = neg;
+      end
+    end
+    else if(alu_op == `SLTIU) begin
+      if (A == B) begin
+        isZero = 0;
+      end
+      else begin
+        isZero = neg;
+      end
+    end
+    else if (alu_op == `F_SLL) begin
       for (j=0;j<B;j=j+1) begin
-          result[j] = 1'b0;
+          R[j] = 1'b0;
       end
       for (j=B;j<`W_CPU;j=j+1) begin
-          result[j] = A[j-B];
+          R[j] = A[j-B];
       end
     end
-    else if (alu_op == `SRL) begin
+    else if (alu_op == `F_SRL) begin
       for (j=0;j<(`W_CPU-B);j=j+1) begin
-          result[j] = A[j+B];
+          R[j] = A[j+B];
       end
       for (j=0;j<B;j=j+1) begin
-          result[j+`W_CPU-B] = A[`W_CPU-1];
+          R[j+`W_CPU-B] = A[`W_CPU-1];
       end
     end
+    else begin
+      R = result;
+    end
   end
-
-  assign R = result;
 endmodule
--- verilog/alu_slice.v
+++ verilog/alu_slice.v
@@ -2,20 +2,20 @@
 `timescale 1ns / 1ps
 
 module ALU_SLICE
- #(parameter DLY = 10)
- (input      [`W_OPCODE-1:0]  alu_op,
-  input      A, B, Cin, //Rs, Rt, Cin
-  output reg R, //Rd/Rt
-  output reg overflow);
+   #(parameter DLY = 10)
+   (input      [`W_OPCODE-1:0]  alu_op,
+    input      A, B, Cin, //Rs, Rt, Cin
+    output reg R, //Rd/Rt
+    output reg overflow);
 
-  wire [2:0] add_;
+  wire [1:0] add_;
   wire and_;
   wire nor_;
   wire or_;
-  wire [2:0] slt_;
+  wire [1:0] slt_;
   wire sll_;
   wire srl_;
-  wire [2:0] sub_;
+  wire [1:0] sub_;
 
   wire add_xor1, add_nand1, add_nand2;// ADD wires
   wire sub_xor1, sub_nand1, sub_nand2, sub_not1;// SUB wires
@@ -47,28 +47,17 @@ module ALU_SLICE
   //AND
     //Literally just a AND (but we have to not and NAND)
     nand #DLY AND_G0(and_not1, A, B);
-    not #DLY AND_G1(and_[0], and_not1);
-    //Give Cout a 0 value since there is none
-    assign and_[1] = 1'b0;
+    not #DLY AND_G1(and_, and_not1);
 
 
   //NOR
     //Literally just a NOR
-    nor #DLY NOR_G0(nor_[0], A, B);
-    //Give Cout a 0 value since there is none
-    assign nor_[1] = 1'b0;
+    nor #DLY NOR_G0(nor_, A, B);
 
   //OR
     //Literally just a OR (but we have to not and NOR)
     nor #DLY OR_G0(or_not1, A, B);
-    not #DLY OR_G1(or_[0], or_not1);
-    //Give Cout a 0 value since there is none
-    assign or_[1] = 1'b0;
-
-
-  assign add_[2] = 1'b0;
-  assign slt_[2] = 1'b0;
-  assign sub_[2] = 1'b0;
+    not #DLY OR_G1(or_, or_not1);
 
 
   //Figure out hwo to do SLL and SRL
@@ -78,23 +67,23 @@ module ALU_SLICE
   //the input is different but that is on the ALU, not the alu_slice
   always @* begin
     case(alu_op)
-      `ADD:  begin R = add_[0]; overflow = add_[1]; end
+      `F_ADD:  begin R = add_[0]; overflow = add_[1]; end
       `ADDI:  begin R = add_[0]; overflow = add_[1]; end
       `ADDIU:  begin R = add_[0]; overflow = add_[1]; end
-      `ADDU:  begin R = add_[0]; overflow = add_[1]; end
-      `AND:  begin R = and_[0]; overflow = 1'b0; end
-      `ANDI:  begin R = and_[0]; overflow = 1'b0; end
-      `NOR:  begin R = nor_[0]; overflow = 1'b0; end
-      `OR:  begin R = or_[0]; overflow = 1'b0; end
-      `ORI:  begin R = or_[0]; overflow = 1'b0; end
-      `SLT:  begin R = sub_[0]; overflow = sub_[1]; end
+      `F_ADDU:  begin R = add_[0]; overflow = add_[1]; end
+      `F_AND:  begin R = and_; overflow = 1'b0; end
+      `ANDI:  begin R = and_; overflow = 1'b0; end
+      `F_NOR:  begin R = nor_; overflow = 1'b0; end
+      `F_OR:  begin R = or_; overflow = 1'b0; end
+      `ORI:  begin R = or_; overflow = 1'b0; end
+      `F_SLT:  begin R = sub_[0]; overflow = sub_[1]; end
       `SLTI:  begin R = sub_[0]; overflow = sub_[1]; end
       `SLTIU:  begin R = sub_[0]; overflow = sub_[1]; end
-      `SLTU:  begin R = sub_[0]; overflow = sub_[1]; end
-      `SLL:  begin R = 1'b0; overflow = 1'b0; end
-      `SRL:  begin R = 1'b0; overflow = 1'b0; end
-      `SUB:  begin R = sub_[0]; overflow = sub_[1]; end
-      `SUBU:  begin R = sub_[0]; overflow = sub_[1]; end
+      `F_SLTU:  begin R = sub_[0]; overflow = sub_[1]; end
+      `F_SLL:  begin R = 1'b0; overflow = 1'b0; end
+      `F_SRL:  begin R = 1'b0; overflow = 1'b0; end
+      `F_SUB:  begin R = sub_[0]; overflow = sub_[1]; end
+      `F_SUBU:  begin R = sub_[0]; overflow = sub_[1]; end
       default : /*Default catch*/;
     endcase
   end
